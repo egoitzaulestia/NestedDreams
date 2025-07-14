26f37fc6a4154b8c9bbf14740b864af0532935b3
IkerPrieto <iprietod01@gmail.com>
2025-07-14 17:54:25 +0200

    navbar y navbar.scss

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
 import Products from "./pages/Products";
+import NavBar from "./components/NavBar";
+import Cart from "./pages/Cart";
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -12,11 +14,13 @@ function App() {
   return (
     <>
       <BrowserRouter>
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/products" element={<Products />} />
+          <Route path="/cart" element={<Cart />} />
           <Route path="/profile" element={<Profile />} />
         </Routes>
       </BrowserRouter>
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,39 @@
+import { Link } from 'react-router-dom'
+import { House, Package, ShoppingCart } from 'lucide-react'
+import '../sass/layout/_navbar.scss'
+
+const NavBar = () => {
+    return (
+        <nav className="navbar">
+            <div className="container">
+                <div className="flex-row-between">
+                    <Link to="/" className="brand">
+                        <div className="logo-box"><span>ND</span></div>
+                        <span className="brand-name">NestedDreams</span>
+                    </Link>
+                    <div className="div-container">
+                        <Link to="/" className="nav-link">
+                            <House size={18} />
+                            <span>Home</span>
+                        </Link>
+                        <Link to="/products" className="nav-link">
+                            <Package size={18} />
+                            <span>Products</span>
+                        </Link>
+                        <Link to="/cart" className="cart-link">
+                            <ShoppingCart size={20} />
+                            {/* {itemCount > 0 && (
+                                <span className="item-count">{itemCount}</span>
+                            )} */}
+                        </Link>
+                        <Link>
+                            <button>Sign In</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
