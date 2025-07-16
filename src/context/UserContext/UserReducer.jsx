13fcba4a4b2a428d444aebb1f21a854b0dc2cdf0
egoitzaulestia <egoitzaulestia@gmail.com>
2025-07-16 16:23:24 +0200

    UserReducer.jsx ready

--- src/context/UserContext/UserReducer.jsx
+++ src/context/UserContext/UserReducer.jsx
@@ -1,3 +1,13 @@
-const users = (state, action) => {};
+const users = (state, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      return {
+        ...state,
+        token: action.payload.token,
+      };
+    default:
+      return state;
+  }
+};
 
 export default users;
