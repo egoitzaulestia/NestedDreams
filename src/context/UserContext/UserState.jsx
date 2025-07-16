279555bcab3d00de892c38085593bbf9930235e5
egoitzaulestia <egoitzaulestia@gmail.com>
2025-07-16 16:20:50 +0200

    UserState.jsx login added

--- src/context/UserContext/UserState.jsx
+++ src/context/UserContext/UserState.jsx
@@ -8,3 +8,33 @@ const initialState = {
   token: tokenStorage ? tokenStorage : null,
   user: null,
 };
+
+const API_URL = "http://localhost:3000";
+
+export const UserContext = createContext(initialState);
+
+export const UserProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(UserReducer, initialState);
+
+  const login = async (user) => {
+    const res = await axios.post(`${API_URL}/users/login`, user);
+
+    dispatch({
+      type: "LOGIN",
+      payload: res.data,
+    });
+    if (res.data) {
+      localStorage.setItem("token", JSON.stringify(res.data.token));
+    }
+  };
+
+  return (
+    <UserContext.Provider
+      value={{
+        token: state.token,
+        user: state.user,
+        login,
+      }}
+    ></UserContext.Provider>
+  );
+};
