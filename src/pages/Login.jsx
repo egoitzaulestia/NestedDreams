caacc0e99ba56e5461e99737ce351ce92e21402a
egoitzaulestia <egoitzaulestia@gmail.com>
2025-07-17 10:55:28 +0200

    login update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -32,6 +32,41 @@ const Login = () => {
                 Welcome back to the nested layers of discovery
               </p>
               <p className="card-description">New to NestedDreams?</p>
+              <div className="container">
+                <Form
+                  name="basic"
+                  labelCol={{ span: 8 }}
+                  wrapperCol={{ span: 16 }}
+                  initialValues={{ remember: true }}
+                  onFinish={onFinish}
+                  onFinishFailed={onFinishFailed}
+                  autoComplete="off"
+                >
+                  <Form.Item
+                    label="Email"
+                    name="email"
+                    rules={[
+                      { required: true, message: "Please input your email" },
+                    ]}
+                  >
+                    <Input />
+                  </Form.Item>
+                  <Form.Item
+                    label="Password"
+                    name="password"
+                    rules={[
+                      { required: true, message: "Please input your password" },
+                    ]}
+                  >
+                    <Input.Password />
+                  </Form.Item>
+                  <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                    <Button type="primary" htmlType="submit">
+                      Submit
+                    </Button>
+                  </Form.Item>
+                </Form>
+              </div>
               <p className="card-description">Create an account</p>
               <p className="card-description">← Back to home</p>
             </div>
@@ -40,41 +75,6 @@ const Login = () => {
       </section>
     </div>
   );
-
-  // return (
-  //   <div className="container">
-  //     <Form
-  //       name="basic"
-  //       labelCol={{ span: 8 }}
-  //       wrapperCol={{ span: 16 }}
-  //       initialValues={{ remember: true }}
-  //       onFinish={onFinish}
-  //       onFinishFailed={onFinishFailed}
-  //       autoComplete="off"
-  //     >
-  //       <Form.Item
-  //         label="Email"
-  //         name="email"
-  //         rules={[{ required: true, message: "Please input your email" }]}
-  //       >
-  //         <Input />
-  //       </Form.Item>
-  //       <Form.Item
-  //         label="Password"
-  //         name="password"
-  //         rules={[{ required: true, message: "Please input your password" }]}
-  //       >
-  //         <Input.Password />
-  //       </Form.Item>
-  //       <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-  //         <Button type="primary" htmlType="submit">
-  //           Submit
-  //         </Button>
-  //       </Form.Item>
-  //     </Form>
-  //     <h1>Login</h1>
-  //   </div>
-  // );
 };
 
 export default Login;
