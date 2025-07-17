687242d73a96e03d4e6d838f78a782c437eb3b57
egoitzaulestia <egoitzaulestia@gmail.com>
2025-07-17 19:54:05 +0200

    Confirm use effect ready

--- src/pages/Confirm.jsx
+++ src/pages/Confirm.jsx
@@ -1,6 +1,30 @@
-import React from "react";
+import { useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { message, Spin } from "antd";
+
+const API_URL = "http://localhost:3000";
 
 const Confirm = () => {
+  const { emailToken } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async () => {
+      try {
+        await axios.get(`${API_URL}/users/confirm/${emailToken}`);
+        message.success("Your email is confirmed! you can now log in");
+      } catch (err) {
+        console.error(err);
+        message.error(
+          err.response?.data?.message || "Confirmation failed or link expired."
+        );
+      } finally {
+        setTimeout(() => navigate("/login"), 1500);
+      }
+    };
+  }, [emailToken, navigate]);
+
   return <div>Confirm</div>;
 };
 
