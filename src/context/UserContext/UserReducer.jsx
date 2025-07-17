d389875765424a64121b2788011733b48d21e218
egoitzaulestia <egoitzaulestia@gmail.com>
2025-07-17 16:08:02 +0200

    get user info user reducer method created

--- src/context/UserContext/UserReducer.jsx
+++ src/context/UserContext/UserReducer.jsx
@@ -4,8 +4,14 @@ const users = (state, action) => {
       return {
         ...state,
         token: action.payload.token,
-        user: action.payload.user,
       };
+
+    case "GET_USER_INFO":
+      return {
+        ...state,
+        user: action.payload,
+      };
+
     default:
       return state;
   }
